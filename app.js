11109f0edec41586c4a661fa1c40cdeb368d9195
Alan Salihi <alansalihi@gmail.com>
2018-12-25 22:58:09 +0100

    adding simple html

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var counter = 0;
+var feedMessage = "initiate..done! ..Ready..";
+var BlankTile = "blank!";
+var durTime = 2000;
+
+// Initiate App
+function App() {
+    document.getElementById("blanktile").innerHTML = BlankTile;
+    document.getElementById("feed").innerHTML = feedMessage;
+ }
